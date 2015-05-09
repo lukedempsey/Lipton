cf11d77321fcef6e135e235d7a83df89ed8c1a05
Luke Dempsey <luke-dempsey@hotmail.com>
2015-05-09 20:03:42 +1000

    Added implementation
    And inherited methods

--- src/aiproj/squatter/squatItLikeItsHot.java
+++ src/aiproj/squatter/squatItLikeItsHot.java
@@ -1,10 +1,43 @@
 package aiproj.squatter;
+
+import java.io.PrintStream;
+
 /**
  * 
  * @author lukedempsey 638407, mason rose-campbell 638370
  *
  */
 
-public class squatItLikeItsHot {
+public class squatItLikeItsHot implements Player, Piece {
+
+	@Override
+	public int init(int n, int p) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Move makeMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int opponentMove(Move m) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getWinner() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void printBoard(PrintStream output) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
